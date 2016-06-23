a5b8eae74e0ce3ac4bd773dd6bff4ed0a1b9f1b2
antone-sb <Anton.Egorov@smartbear.com>
2016-06-23 14:54:46 +0300

    Change dialog description

--- src/main/java/com/smartbear/postman/ImportPostmanCollectionAction.java
+++ src/main/java/com/smartbear/postman/ImportPostmanCollectionAction.java
@@ -26,30 +26,21 @@ public class ImportPostmanCollectionAction extends AbstractSoapUIAction<Workspac
     public void perform(Workspace workspace, Object param) {
         if (dialog == null) {
             dialog = ADialogBuilder.buildDialog(Form.class);
-//            dialog.setValue(Form.TYPE, RESOURCE_LISTING_TYPE);
         } else {
             dialog.setValue(Form.POSTMAN_COLLECTION_FILE, "");
         }
 
         while (dialog.show()) {
             try {
-                // get the specified URL
                 String fieldValue = dialog.getValue(Form.POSTMAN_COLLECTION_FILE);
                 if (StringUtils.hasContent(fieldValue)) {
                     String filePath = fieldValue.trim();
                     if (StringUtils.hasContent(filePath)) {
-//                    // expand any property-expansions
-//                    String expUrl = PathUtils.expandPath(filePath, project);
-
-                        // if this is a file - convert it to a file URL
                         if (new File(filePath).exists()) {
                             PostmanImporter importer = new PostmanImporter(new GuiTestCreator());
                             WsdlProject project = importer.importPostmanCollection(filePath);
                             workspace.addProject(project);
                         }
-
-//                    SwaggerImporter importer = SwaggerUtils.importSwaggerFromUrl(project, expUrl, dialog.getValue(Form.TYPE).equals(RESOURCE_LISTING_TYPE));
-//                    Analytics.trackAction("ImportSwagger", "Importer", importer.getClass().getSimpleName());
                         break;
                     }
                 }
@@ -59,14 +50,11 @@ public class ImportPostmanCollectionAction extends AbstractSoapUIAction<Workspac
         }
     }
 
-    @AForm(name = "Import Postman Collection", description = "Creates a project from the specified Postman collection")
+    @AForm(name = "Import Postman Collection", description = "Create a project from the specified Postman collection")
     public interface Form {
         @AField(name = "Postman Collection", description = "Location or Postman collection file", type = AFieldType.FILE)
         String POSTMAN_COLLECTION_FILE = "Postman Collection";
 
-//        @AField(name = "Definition Type", description = "Resource Listing or API Declaration",
-//                type = AFieldType.RADIOGROUP, values = {RESOURCE_LISTING_TYPE, API_DECLARATION_TYPE})
-//        public final static String TYPE = "Definition Type";
     }
 }
 
