15e668c8961222ac9315172c948d04f14dddb581
Ryszard Precikowski <ryszard.precikowski@smartbear.com>
2023-07-11 15:53:40 +0200

    RIA-21054 create DirectoryInfo class

---
+++ src/main/java/com/smartbear/postman/collection/DirectoryInfo.java
@@ -0,0 +1,33 @@
+package com.smartbear.postman.collection;
+
+public class DirectoryInfo {
+    private final String name;
+    private final String description;
+
+    private DirectoryInfo parent;
+
+    public DirectoryInfo(String name, String description, DirectoryInfo parent) {
+        this.name = name;
+        this.description = description;
+        this.parent = parent;
+    }
+
+    public static DirectoryInfo createRoot(String collectionName){
+        return new DirectoryInfo(collectionName, "", null);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPath() {
+        if (parent == null) {
+            return name;
+        }
+        return parent.getPath() + "/" + name;
+    }
+}
