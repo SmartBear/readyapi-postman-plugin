b02b5831d251d4e6dff19d207148db04e259f692
Marharyta Panska <marharyta.panska@smartbear.com>
2023-09-04 15:14:31 +0200

    RIA-21055 Change collection version check process

---
+++ src/main/java/com/smartbear/postman/collection/PostmanCollectionV2_1.java
@@ -0,0 +1,9 @@
+package com.smartbear.postman.collection;
+
+import net.sf.json.JSONObject;
+
+public class PostmanCollectionV2_1 extends PostmanCollectionV2 {
+    public PostmanCollectionV2_1(JSONObject postmanCollection) {
+        super(postmanCollection);
+    }
+}
