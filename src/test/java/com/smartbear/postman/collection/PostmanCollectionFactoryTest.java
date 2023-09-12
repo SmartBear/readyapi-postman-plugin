760da8db0c9a6bd29678a20a4a77b23ef47c11ad
Marharyta Panska <marharyta.panska@smartbear.com>
2023-09-12 13:50:47 +0200

    RIA-21055 Remove collection V1 support and cleanup existing tests

--- src/test/java/com/smartbear/postman/collection/PostmanCollectionFactoryTest.java
+++ src/test/java/com/smartbear/postman/collection/PostmanCollectionFactoryTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,12 +21,12 @@ public class PostmanCollectionFactoryTest {
     private final String COLLECTION_FROM_URL_PATH = "/Postman_Echo_from_url.postman_collection_v2.1";
 
     @Test(expected = PostmanCollectionUnsupportedVersionException.class)
-    public void postmanCollectionV1ImportIsNull() throws IOException, PostmanCollectionUnsupportedVersionException {
+    public void postmanCollectionV1ImportThrowsUnsupportedVersionException() throws Exception {
         getCollectionFromFile(getClass().getResource(COLLECTION_V1_PATH).getPath());
     }
 
     @Test
-    public void postmanCollectionV2ImportsAsV2() throws IOException, PostmanCollectionUnsupportedVersionException {
+    public void postmanCollectionV2ImportsAsV2() throws Exception {
         PostmanCollection postmanCollection = getCollectionFromFile(
                 getClass().getResource(COLLECTION_V2_PATH).getPath()
         );
@@ -35,7 +34,7 @@ public class PostmanCollectionFactoryTest {
     }
 
     @Test
-    public void postmanCollectionV2_1ImportsAsV2() throws IOException, PostmanCollectionUnsupportedVersionException {
+    public void postmanCollectionV2_1ImportsAsV2() throws Exception {
         PostmanCollection postmanCollection = getCollectionFromFile(
                 getClass().getResource(COLLECTION_V2_1PATH).getPath()
         );
@@ -43,14 +42,14 @@ public class PostmanCollectionFactoryTest {
     }
 
     @Test
-    public void postmanCollectionV2_1FromURLImportsAsV2() throws IOException, PostmanCollectionUnsupportedVersionException {
+    public void postmanCollectionV2_1FromURLImportsAsV2() throws Exception {
         PostmanCollection postmanCollection = getCollectionFromFile(
                 getClass().getResource(COLLECTION_FROM_URL_PATH).getPath()
         );
         assertThat(postmanCollection, instanceOf(PostmanCollectionV2.class));
     }
 
-    private PostmanCollection getCollectionFromFile(String collectionFilePath) throws IOException, PostmanCollectionUnsupportedVersionException {
+    private PostmanCollection getCollectionFromFile(String collectionFilePath) throws Exception {
         String postmanJson = FileUtils.readFileToString(new File(collectionFilePath), StandardCharsets.UTF_8);
         JSON json = new PostmanJsonUtil().parseTrimmedText(postmanJson);
         return PostmanCollectionFactory.getCollection((JSONObject) json);
