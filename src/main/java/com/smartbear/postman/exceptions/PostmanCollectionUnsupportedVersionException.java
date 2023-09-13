95da449a22dade86fe1f2dfaa288a657fd2dd885
Marharyta Panska <marharyta.panska@smartbear.com>
2023-09-13 12:16:16 +0200

    RIA-21055 More details in exception message

--- src/main/java/com/smartbear/postman/exceptions/PostmanCollectionUnsupportedVersionException.java
+++ src/main/java/com/smartbear/postman/exceptions/PostmanCollectionUnsupportedVersionException.java
@@ -2,7 +2,10 @@ package com.smartbear.postman.exceptions;
 
 public class PostmanCollectionUnsupportedVersionException extends Exception {
 
-    public static final String UNSUPPORTED_VERSION_MESSAGE = "Unsupported version of Postman collection was provided";
+    public static final String UNSUPPORTED_VERSION_MESSAGE = "Unsupported version of Postman collection was provided.\n" +
+            "Make sure your collection version is 2.0.0 or 2.1.0 (recommended). " +
+            "Try importing the collection into Postman and exporting in one of the supported formats.\n" +
+            "ReadyAPI Postman Plugin documentation: https://support.smartbear.com/readyapi/docs/integrations/postman.html";
 
     public PostmanCollectionUnsupportedVersionException(String msg) {
         super(msg);
