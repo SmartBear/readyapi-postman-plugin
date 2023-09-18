225e02bb3cb6e3a52b4f5ccb47d3a58688787c84
Marharyta Panska <marharyta.panska@smartbear.com>
2023-09-18 15:57:28 +0200

    RIA-21055 Add collection version regex

--- src/main/java/com/smartbear/postman/collection/PostmanCollectionFactory.java
+++ src/main/java/com/smartbear/postman/collection/PostmanCollectionFactory.java
@@ -2,15 +2,17 @@ package com.smartbear.postman.collection;
 
 import com.smartbear.postman.exceptions.PostmanCollectionUnsupportedVersionException;
 import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringUtils;
+import java.util.Arrays;
+import java.util.Optional;
 import static com.smartbear.postman.exceptions.PostmanCollectionUnsupportedVersionException.UNSUPPORTED_VERSION_MESSAGE;
 
 public class PostmanCollectionFactory {
     public static final String INFO = "info";
     public static final String COLLECTION = "collection";
     public static final String SCHEMA = "schema";
-    public static final String VERSION_2 = "2.0.0";
-    public static final String VERSION_2_1 = "2.1.0";
+    public static final String VERSION_2 = "v2.0.0";
+    public static final String VERSION_2_1 = "v2.1.0";
+    public static final String VERSION_REGEX = "v(?!\\.)(\\d+(\\.\\d+)+)(?![\\d\\.])$";
 
     public static PostmanCollection getCollection(JSONObject collectionObject) throws PostmanCollectionUnsupportedVersionException {
         Object collectionSharedViaAPI = collectionObject.get(COLLECTION);
@@ -25,9 +27,12 @@ public class PostmanCollectionFactory {
         if (infoSection != null) {
             Object schemaField = ((JSONObject) infoSection).get(SCHEMA);
             if (schemaField instanceof String) {
-                String collectionVersion = StringUtils
-                        .substringBetween((String) schemaField, "collection/v", "/collection.json");
-                if (VERSION_2.equals(collectionVersion) || VERSION_2_1.equals(collectionVersion)) {
+                Optional<String> collectionVersion = Arrays
+                        .stream(((String) schemaField).split("/"))
+                        .filter(schemaPart -> schemaPart.matches(VERSION_REGEX)
+                                && (VERSION_2.equals(schemaPart) || VERSION_2_1.equals(schemaPart)))
+                        .findAny();
+                if (collectionVersion.isPresent()) {
                     return new PostmanCollectionV2(collectionObject);
                 }
             }
