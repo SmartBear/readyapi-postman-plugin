591d309914faac1ffaf5b1d802fbf4d92ca0f3b0
Marharyta Panska <marharyta.panska@smartbear.com>
2025-01-29 13:49:15 +0100

    RIA-23676 Unit test for secret variables

--- src/test/java/com/smartbear/ready/plugin/postman/ImportPostmanEnvironmentActionTest.java
+++ src/test/java/com/smartbear/ready/plugin/postman/ImportPostmanEnvironmentActionTest.java
@@ -1,6 +1,9 @@
 package com.smartbear.ready.plugin.postman;
 
 import com.eviware.soapui.impl.wsdl.WsdlProjectPro;
+import com.eviware.soapui.model.testsuite.EncryptableTestProperty;
+import com.eviware.soapui.model.testsuite.TestProperty;
+import com.eviware.soapui.settings.ProjectSettings;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.smartbear.ready.plugin.postman.collection.environment.PostmanEnvModel;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,15 +14,16 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ImportPostmanEnvironmentActionTest {
 
     private static final String ENV_WITH_DUPLICATE_VALUES = "/environments/env_with_duplicate_value_names.postman_environment.json";
+    private static final String ENV_WITH_SECRET_VALUES = "/environments/env_with_secret_values.postman_environment.json";
     private static final String JSON_WITH_RANDOM_DATA = "/environments/invalid_format_env.json";
     private static final String TEXT_FILE = "/environments/random.txt";
 
@@ -36,7 +40,7 @@ class ImportPostmanEnvironmentActionTest {
     void testIfEnvironmentWasCreated() throws IOException, URISyntaxException {
         PostmanEnvModel postmanEnvModel = loadEnvironmentFromFile(ENV_WITH_DUPLICATE_VALUES);
         importPostmanEnvironmentAction.createEnvironmentAndPopulateFrom(postmanEnvModel);
-        assertThat(project.getEnvironmentByName(postmanEnvModel.getName()), notNullValue());
+        assertNotNull(project.getEnvironmentByName(postmanEnvModel.getName()));
     }
 
     @Test
@@ -63,6 +67,22 @@ class ImportPostmanEnvironmentActionTest {
     @Test
     void shouldFailOnNotAJsonFile() {
         assertThrows(JsonParseException.class, () -> loadEnvironmentFromFile(TEXT_FILE));
+    }
+
+    @Test
+    void testSecretPropertiesEncryption() throws IOException, URISyntaxException {
+        project.getSettings().setString(ProjectSettings.SHADOW_PASSWORD, "password");
+
+        PostmanEnvModel postmanEnvModel = loadEnvironmentFromFile(ENV_WITH_SECRET_VALUES);
+        importPostmanEnvironmentAction.createEnvironmentAndPopulateFrom(postmanEnvModel);
+
+        postmanEnvModel.getValues().forEach(variable -> {
+            TestProperty testProperty = project.getProjectProperty(variable.getKey());
+            if (variable.isSecret()) {
+                assertTrue(((EncryptableTestProperty) testProperty).isEncrypted());
+            }
+        });
+        project.getSettings().setString(ProjectSettings.SHADOW_PASSWORD, null);
     }
 
     private PostmanEnvModel loadEnvironmentFromFile(String fileLocation) throws IOException, URISyntaxException {
